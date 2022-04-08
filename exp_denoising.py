c70ae62a0425052cc10cad2ffe05d55b19b3d813
zineblahrichi <zineb.lahrichi@student-cs.fr>
2022-04-08 12:09:30 +0200

    Fixed denoising experiments

--- exp_denoising.py
+++ exp_denoising.py
@@ -3,7 +3,7 @@ from tools import *
 from utils import create_scribble, compute_distance, compute_segmentation
 from utils import open_img, read_ground_truth, label2rgb
 from default_parameters import *
-from skimage.restoration import denoise_bilateral, denoise_wavelet
+from skimage.restoration import denoise_bilateral
 
 
 script_dir = os.path.dirname(__file__)
@@ -11,27 +11,31 @@ input_dir = os.path.join(script_dir, 'input/')
 
 if __name__ == '__main__':
 
-    img_name = 'cow'
+    img_name = 'boat'
+    NB_CLASSES = 9
 
+    NB_ITERS = 10
+    LMBD = 0.1
+    alpha = 100
+    sigma = 40
+
+    default_shape = open_img(input_dir + img_name + '.jpg', None).shape[:2]
     img = open_img(input_dir + img_name + '.jpg', down_shape)
-    denoised_wavelet = denoise_wavelet(img, channel_axis=-1, sigma=0.2)
-    denoised_bilateral = denoise_bilateral(img, channel_axis=-1, sigma_color=5, sigma_spatial=2)
-    smoothed_images = [denoised_wavelet, denoised_bilateral]
-    denoising_methods = ['wavelet', 'bilateral']
-    NB_CLASSES = 6
+    denoised_bilateral = denoise_bilateral(img, channel_axis=-1, sigma_color=1, sigma_spatial=1, mode='edge')
+    denoising_methods = ['original', 'pre-denoising']
+    smoothed_images = [img, denoised_bilateral]
 
     bests = []
-    for img in smoothed_images:
-        img = img / np.max(img) * 255.
+    for img2 in smoothed_images:
+        img2 = img2 / np.max(img2) * 255.
 
-        scribbles, X, Y, I = create_scribble(img, img_name, input_dir, showScribble, NB_CLASSES)
+        scribbles, X, Y, I = create_scribble(img2, img_name, input_dir, showScribble, NB_CLASSES)
 
         print('>> Computing shortest path to scribble...')
-        n, m = img.shape[:2]
-        dists = compute_distance(img, X, Y, NB_CLASSES, showDistanceMap)
+        dists = compute_distance(img2, X, Y, NB_CLASSES, showDistanceMap)
 
         print('>> Computing segmentation...')
-        best = compute_segmentation(img, scribbles, X, Y, I, dists, alpha, eps, sigma, USE_DIST, USE_COLOR, MARGINAL,
+        best = compute_segmentation(img2, scribbles, X, Y, I, dists, alpha, eps, sigma, USE_DIST, USE_COLOR, MARGINAL,
                                     NB_CLASSES, NB_ITERS, LMBD)
         bests.append(best)
 
@@ -39,12 +43,22 @@ if __name__ == '__main__':
     gt = read_ground_truth(ground_truth_filepath, default_shape)
 
     plt.figure(figsize=(20, 15))
-    fig, axes = plt.subplots(1, len(smoothed_images))
-    for ax, best, method in zip(axes, bests, denoising_methods):
-        ax.imshow(label2rgb(best, NB_CLASSES))
-        ax.title.set_text(method)
-    axes[-1].imshow(label2rgb(gt, NB_CLASSES))
+    fig, axes = plt.subplots(len(smoothed_images) + 1, 3)
+
+    for ax, best, method, original_img in zip(axes, bests, denoising_methods, smoothed_images):
+
+        ax[0].imshow(original_img)
+        ax[0].title.set_text(method)
+
+        segmented = label2rgb(best, NB_CLASSES)
+        ax[1].imshow(segmented)
+        ax[1].title.set_text(method + ' + segmentation')
+
+    axes[-1][0].imshow(label2rgb(gt, NB_CLASSES))
+    axes[-1][1].imshow(label2rgb(gt, NB_CLASSES))
     plt.show()
+
+
 
 
 
