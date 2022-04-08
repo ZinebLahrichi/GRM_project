18925be152150d0df5ebea4cc1c54e4117bfe8bb
zineblahrichi <zineb.lahrichi@student-cs.fr>
2022-04-08 12:10:01 +0200

    Fixed experiments on parameter alpha

--- exp_alpha.py
+++ exp_alpha.py
@@ -7,20 +7,19 @@ from default_parameters import *
 script_dir = os.path.dirname(__file__)
 input_dir = os.path.join(script_dir, 'input/')
 
-alphas = [1, 10, 100, 500]
+alphas = [0.0001, 0.1, 10, 1000]
 
 if __name__ == '__main__':
 
     img_name = 'cow'
 
-    img = open_img(input_dir + img_name + '.jpg', down_shape)
+    img = open_img(input_dir + img_name + '.jpg', default_shape)
     img = img / np.max(img) * 255.
     NB_CLASSES = 6
 
     scribbles, X, Y, I = create_scribble(img, img_name, input_dir, showScribble, NB_CLASSES)
 
     print('>> Computing shortest path to scribble...')
-    n, m = img.shape[:2]
     dists = compute_distance(img, X, Y, NB_CLASSES, showDistanceMap)
 
     bests = []
@@ -28,6 +27,7 @@ if __name__ == '__main__':
         print('>> Computing segmentation...')
         best = compute_segmentation(img, scribbles, X, Y, I, dists, alpha, eps, sigma, USE_DIST, USE_COLOR, MARGINAL,
                                     NB_CLASSES, NB_ITERS, LMBD)
+        bests.append(best)
 
     ground_truth_filepath = input_dir + img_name + '_layers.txt'
     gt = read_ground_truth(ground_truth_filepath, default_shape)
